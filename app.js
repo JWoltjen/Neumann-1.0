35fd2693f0c68fb7936156ea7b3d4a246898970e
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-02-08 15:04:26 -0600

    add computer speech to message object

--- app.js
+++ app.js
@@ -13,9 +13,20 @@ recognition.onresult = function(event) {
     const current = event.resultIndex; 
 
     const transcript = event.results[current][0].transcript; 
-    content.textContent = transcript
+    content.textContent = transcript; 
+    readOutLoud(transcript); 
 }
 
 btn.addEventListener('click', () => {
     recognition.start(); 
 })
+
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance(); 
+    speech.text = message; 
+    speech.volume = 1; 
+    speech.rate = .7; 
+    speech.pitch = .7; 
+
+    window.speechSynthesis.speak(speech); 
+}
