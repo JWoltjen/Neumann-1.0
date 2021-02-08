e23a289208c7d1111b6625a2d95922e439da8dc7
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-02-08 14:46:11 -0600

    build speech recognition listener

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = documemnt.querySelector('.talk'); 
+const content = document.querySelector('.content'); 
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; 
+const recognition = new SpeechRecognition(); 
+
+recognition.onstart = function () {
+    console.log(`Go ahead, i'm listening...`)
+}
