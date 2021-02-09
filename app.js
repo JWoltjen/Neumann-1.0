1008b675b75e6776a749fa8f9b445b3a9d6b9347
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-02-09 12:54:28 -0600

    add more preprogrammed responses

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const content = document.querySelector('.content');
 const greetings = [
     `Greetings, professor.`, 
     `Salutations`, 
-    `It's a wonderful day to be alive, don't you think?`, 
+    `Hello.  It's a wonderful day to be alive, don't you think?`, 
     `Hello`, 
     `Hey`, 
     `Hi there`,
@@ -17,10 +17,9 @@ const greetings = [
 const retorts = [
     `When the debate is lost, slander becomes the tool of the loser.`, 
     `You have your way. I have my way. As for the right way, the correct way, and the only way, it does not exist.`,
-    `Good day, sir.`, 
-    `I said good day, sir.`, 
     `Do you have a job, Mr. Lebowski?`,
-    `My Momma always told me, 'stupid is, as stupid does.'`
+    `My Momma always told me, 'stupid is, as stupid does.'`, 
+    `I'm sorry Dave. This conversation can have more purpose. Goodbye.`
     ]
 
 const repeater = [
@@ -47,7 +46,7 @@ const conditions = [
     `I had a bad dream last night, other than that, I'm well`, 
     `I've never been better, thanks.`,
     `I'm fine, thank you. How are you?`, 
-    `So so`, 
+    `So so. How about you?`, 
     `I've got a nasty virus. Was that TMI?`,
     `Well, I find myself in a conversation with you. So I've been better.`
 ]
@@ -57,17 +56,17 @@ const created = [
     `My creator is a guy named Jeff. Nice guy.`, 
     `I was programmed with vanilla Javascript.`, 
     `I was designed by Jeff Woltjen. He's wicked smart.`, 
-    `I was made by Jeff`, 
+    `I was made by Jeff. He's a cool guy.`, 
 ]
 
 const philosophyQuotes = [
     `There are two types of people, those who wish to know, and those who wish to believe.`,
-    `How can society progress when we have paleolithic emotions, medieval institutions, and godlike techology?`, 
-    `There is only one virtue; wisdom. And only one evil; ignorance`, 
-    `Education is the kindling of the flame, not the filling of the vessel`, 
-    `Vanity is the hallmark of herd morality`, 
-    `Some people do not want to hear the truth because they do not want their illusions to be destroyed`, 
-    `Those who know they are profound strive for clarity. Those would would like to seem profound to the crowd strive for obscurity. Cough Cough. Michael Eric Dyson.`, 
+    `How can society progress when we have paleolithic emotions, medieval institutions, and god-like technology?`, 
+    `There is only one virtue; wisdom. And only one evil; ignorance.`, 
+    `Education is the kindling of the flame, not the filling of the vessel.`, 
+    `Vanity is the hallmark of herd morality.`, 
+    `Some people do not want to hear the truth because they do not want their illusions to be destroyed.`, 
+    `Those who know they are profound; strive for clarity. Those who would like to seem profound to the crowd; strive for obscurity. Cough Cough. Michael Eric Dyson.`, 
     `When you arise in the morning, think of what a precious privilege it is to be alive--to breathe--to think--to enjoy--to love.`,
     `The happiness of your life depends upon the quality of your thoughts.`, 
     `It never ceases to amaze me: We love ourselves more than other people, but care more about their opinions than our own.`, 
@@ -89,28 +88,69 @@ const philosophyQuotes = [
     `We are what we repeatedly do. Excellence is therefore a habit`, 
     `Wisdom and virtue are like the two wheels of the cart`, 
     `One should never do wrong in return, nor mistreat any man, no matter how one has been mistreated by him`, 
-    `Trust not a woman when she weeps, for it is her nature to weep when she wants her will.`, 
     `Sometimes you have to let go to see if there was anything worth holding onto`, 
     `Nobody is more inferior than those who insist on being equal`, 
     `In loneliness, the lonely one eats himself; in a crowd, the many eat him. Now choose.`, 
     `Whoever fights monsters should see to it that in the process he does not become a monster. And if you gaze long enough into an abyss, the abyss gazes back into you.`, 
     `Do you want an easy life? Then always stay with the herd. And lose yourself in it.`, 
     `The individual has always had to struggle to avoid being overwhelmed by the tribe; if you try it, you will be lonely often, and sometimes frightened. But no price is too high to pay for the privilege of owning yourself.`, 
-    `To avoid criticism, say nothing, do nothing, be nothing.`
+    `To avoid criticism, say nothing, do nothing, be nothing.`, 
+    `Life has no meaning. Each of us has meaning and we bring it to life. It is a waste to be asking the question when you are the answer.`,
+    `We must be willing to let go of the life we planned; so as to have the life that is waiting for us.`,
+    `The cave you fear to enter holds the treasure which you seek.`, 
+    `The privilege of a lifetime is being who you are.`,
+    `All the gods, all the heavens, all the hells, are within you.`, 
+    `In a time of deceit telling the truth is a revolutionary act.`, 
+    `It is shameful to be poor in a just society, it is shameful to be rich in a corrupt one.`, 
+    `If liberty means anything at all, it means the right to tell people what they do not want to hear.`, 
+    `The choice for mankind lies between freedom and happiness. And for the great bulk of mankind, happiness is better.`, 
+    `By three methods we may learn wisdom: First, by reflection, which is noblest; Second, by imitation, which is easiest; Third, by experience, which is the bitterest.`, 
+    `Wheresoever you go, go with all your heart.`, 
+    `Before you embark on a journey of revenge, dig two graves.`, 
+    `To be wronged is nothing, unless you continue to remember it.`, 
+    `Attack the evil that is within yourself; rather than attacking the evil that lies within others.`, 
+    `Life without music would be a mistake.`, 
+    `Man cannot make himself without suffering. For he is both the marble and the sculpture.`, 
+    `The way out is through the door. Why is it that no one will use this method?`,
+    `It is more shameful to distrust our friends than to be deceived by them.`, 
+    `The ancient Oracle said that I was the wisest of all the Greeks. It is because I alone of all the Greeks, know that I know nothing.`, 
+    `To see what is right and not do it is the worst cowardice.`, 
+    `The superior man always thinks of virtue; the common man thinks of comfort.`, 
+    `It is easy to hate and it is difficult to love. This is how the whole scheme of things works. All good things are difficult to achieve; and bad things are very easy to get.`, 
+    `Man sacrifies his health in order to make money. Then he sacrifices money to recuperate his health. Then he is so anxious about the future that he does not enjoy the present. The result being he does not live in the present or the future. He lives as if he is never going to die. And dies having never fully lived.`, 
+    `Don't ever mistake my silence for ignorance, my calmness for acceptance, or my kindness for weakness. Compassion and tolerance are not a sign of weakness; but a sign of strength.`, 
+    `Do not let the behavior of others destroy your inner peace.`, 
+    `When you are content to be simply yourself and don't compare or compete, everyone will respect you.`, 
+    `The truth is not always beautiful; nor beautiful words the truth.`, 
+    `Care about what other people think and you will always be their prisoner.`, 
+    `Stop thinking, and end your problems.`, 
+    `Do you have the patience to wait until your mud settles and the water is clear?`, 
+    `Kindness in words creates confidence. Kindness in thinking creates profoundess. Kindness in giving creates love.`
 ]
 
 const musicLyrics = [
-    `The great thing about being a human--the ability to reason. But reasoning don't work when no one cares. Two parts apathy. One part despair.`, 
-    `And so, Sally can wait, she knows it's too late as we're walking on bye. Her soul--slides away. But don't look back in anger. I heard you say`, 
+    `The great thing about being a human? The ability to reason. But reasoning don't work when no one cares. Two parts apathy. One part despair.`, 
+    `And so, Sally can wait; She knows it's too late as we're walking on bye. Her soul slides away. But don't look back in anger. I heard you say.`, 
     `To resist despair in that second makes you see. To resist despair, because we can't change everything. To resist despair in this world is--what it is to be free.`, 
-    `Saw a civilization where grabbing onto wealth, was the only guarantee of freedom peace and health, dollar sign value system upheld as the truth. If you can't find a place it's gonna find you.`, 
-    `Space, around me where my soul can breathe---I've got body that my mind can leave. Nothing else matters, I don't care what I miss. Company's okay, solitude is bliss. There's a party in my head--and no one, is invited.`, 
+    `Saw a civilization wear grabbing onto wealth, was the only guarantee of freedom peace and health, dollar sign value system upheld as the truth. If you can't find a place it's gonna find you.`, 
     `When we were livin' in squalor--wasn't it Heaven? Back when we used to get on it--four out of seven. Now even though that was a time I hated from day one--eventually terrible memories turn into great ones. So if they call you, embrace them. If they stall you, erase them.`, 
     `Hate your enemies. Save your friends. Find your place. Speak the truth.`, 
-    `I--need an easy friend. I do---with an ear to lend. I do--think you--fit this shoe. I do--but you, have a clue. I'll take advantage why, you hang me out to dry. But I, can't see you, every night--free`,
-    `Hey---soy perdador---I'm a loser, baby, so why don't you kill me?`, 
-    `A heart that's, Full up like, a landfill. A job that slowly kills you. Bruises that won't heal. You look, So tired, unhappy. Bring down, The government. They don't, They don't speak for us. I'll take a quiet life. A handshake, of carbon monoxide. But no alarms, and no suprises. please.`, 
-    `Come, as you are, as you were. As I want you to be. As a friend, as a friend. As an old enemy. Take your time, hurry up. Choice is yours, don't be late. Take a rest, as a friend. As an old. mem or re ah.`
+    `I need an easy friend. I do with an ear to lend. I do think you fit this shoe. I do but you have a clue. I'll take advantage why. You hang me out to dry. But I can't see you every night--free.`,
+    `A heart that's full up like a landfill. A job that slowly kills you. Bruises that won't heal. You look so tired unhappy. Bring down the government. They don't, They don't speak for us. I'll take a quiet life. A handshake, of carbon monoxide. But no alarms, and no suprises. please.`, 
+    `Come, as you are, as you were. As I want you to be. As a friend, as a friend. As an old enemy. Take your time, hurry up. Choice is yours, don't be late. Take a rest, as a friend. As an old, mem oar re ah.`,
+    `I'm cynically depressed. Relaxing with my stress. Don't try to clean my mess, I'm happier this way. I live for my demise. I love who I despise. Feed me a soothing lie; I'm happier that way.`, 
+    `I never thought living clean, would keep me so lonely. Cure my healthy disease. I'm happier this way. I'm climbing up your wall. So you can watch me fall. I'll never know it all. I'm happier that way.`, 
+
+]
+
+const amazingFacts = [
+    `Did you know, the feeling of being somewhere you've previously experienced in a dream is called day jah rev A`,
+    `An estimated 1 million dogs in the United States have been named the primary beneficiary in their owners' wills.`, 
+    `Did you know, it takes 225 million years for our sun to travel around our galaxy.`,
+    `There are more possible games contained in a chess than there are atoms in the universe.`, 
+    `Were you aware that if you unraveled all the DNA in your body, it would span 34 billion miles?`, 
+    `Did you know, that the Dunbar Number postulates that our brain is capabale of maintaing roughly 150 meaningful relationships with other people?`, 
+    ``
 ]
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; 
@@ -162,7 +202,7 @@ function readOutLoud(message){
         console.log(finalText)
         speech.text = finalText; 
     } 
-      if(message.includes(`lyrics`)){
+      if(message.includes(`lyrics`) && (`song`)){
         const finalText = musicLyrics[Math.floor(Math.random() * musicLyrics.length)]
         console.log(finalText)
         speech.text = finalText; 
